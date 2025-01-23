d6f3c036daf0d1c134451483122ea96758e3b03d
Justice8904 <joao.cavassini12@gmail.com>
2025-01-23 11:00:54 -0400

    Teste2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,40 @@
+<script>
 
-        <nav id="barra" class="navbar sticky-top navbar-expand-lg">
+</script>
 
-            <div class="container-fluid">
-                <a id="botaoMenu" class="navbar-brand" href="Menu.html">Dimensions</a>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="container" id="btnContainer">
-                        <a id="btnNavbar" class="link-underline link-underline-opacity-0" href="Ficha.html">Nova Ficha</a>
-                        <a href="https://docs.google.com/document/d/1VUflHNDV1XWF0sWB_IBh5LEw95fY0_6UBnlDWS928BY/edit?tab=t.0#heading=h.y28yvhaxxjbd" class="link-underline link-underline-opacity-0" id="btnNavbar">Livro</a>
-                        <a href="Anotacoes.html" class="link-underline link-underline-opacity-0" id="btnNavbar">Anotações</a>
-
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-    
-   
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+</nav>
