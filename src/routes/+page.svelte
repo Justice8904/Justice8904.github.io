a197577a880dfa1542ad597c85281cafdbc9c4ae
Justice8904 <joao.cavassini12@gmail.com>
2025-01-23 10:48:26 -0400

    Teste

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+        <nav id="barra" class="navbar sticky-top navbar-expand-lg">
+
+            <div class="container-fluid">
+                <a id="botaoMenu" class="navbar-brand" href="Menu.html">Dimensions</a>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="container" id="btnContainer">
+                        <a id="btnNavbar" class="link-underline link-underline-opacity-0" href="Ficha.html">Nova Ficha</a>
+                        <a href="https://docs.google.com/document/d/1VUflHNDV1XWF0sWB_IBh5LEw95fY0_6UBnlDWS928BY/edit?tab=t.0#heading=h.y28yvhaxxjbd" class="link-underline link-underline-opacity-0" id="btnNavbar">Livro</a>
+                        <a href="Anotacoes.html" class="link-underline link-underline-opacity-0" id="btnNavbar">Anotações</a>
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    
+   
